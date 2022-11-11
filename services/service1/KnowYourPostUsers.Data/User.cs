651a621a0841507458b0e44e5770c92caf957641
Larry <66905431+larry-oj@users.noreply.github.com>
2022-11-11 18:27:38 +0200

    service1 - database

---
+++ services/service1/KnowYourPostUsers.Data/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KnowYourPostUsers.Data;
+
+public class User
+{
+    [Key] [DatabaseGenerated(DatabaseGeneratedOption.Identity)] public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }       // is unique
+    public string Password { get; set; }    // forbidden to store unhashed, never do this in real-life scenarios
+}
