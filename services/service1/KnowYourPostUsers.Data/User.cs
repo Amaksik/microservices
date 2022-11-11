d51498d52c3dc89e15b4935841c22161e6aceae9
Larry <66905431+larry-oj@users.noreply.github.com>
2022-11-11 18:56:11 +0200

    service1 - remove user name field

--- services/service1/KnowYourPostUsers.Data/User.cs
+++ services/service1/KnowYourPostUsers.Data/User.cs
@@ -6,7 +6,12 @@ namespace KnowYourPostUsers.Data;
 public class User
 {
     [Key] [DatabaseGenerated(DatabaseGeneratedOption.Identity)] public int Id { get; set; }
-    public string Name { get; set; }
     public string Email { get; set; }       // is unique
     public string Password { get; set; }    // forbidden to store unhashed, never do this in real-life scenarios
+
+    public User(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
 }
